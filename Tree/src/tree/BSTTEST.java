d8fbdad716f83bf8a8829d9d37e17053c83fd0c4
Stanley Kwok <43926763+whybbcry@users.noreply.github.com>
2018-11-26 17:27:27 +0800

    Add files via upload

---
+++ Tree/src/tree/BSTTEST.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tree;
+
+/**
+ *
+ * @author Stanley Kwok 187100835
+ */
+
+//Q8
+
+class BSTTest {
+    public static void main(String [] args) {
+       int data = 0;
+       BinarySearchTree tree = new BinarySearchTree();
+     
+       System.out.println("Inserting :");
+       
+       int [] sequence = {2,7,9,4,1,5,3,6,0,8};
+    
+    for (int i = 0; i < sequence.length; i++) {
+            System.out.print(sequence[i] + " ");
+            tree.insert(sequence[i]);
+        }
+
+    
+    System.out.println();
+    //PRINT THE SEQUENCE AGAIN IN ASCENDING ORDER
+    System.out.println("In ascending order:");
+    tree.inorder();   
+   }
+}
